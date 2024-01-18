5bcb904a51a26c8c4bf3c4c20faa87a5e877df4e
shary2023 <128229183+shary2023@users.noreply.github.com>
2024-01-18 22:16:04 +0600

    correct proxy

--- src/patterns/structural/wrappers/proxy/solution/PictureDownloadServiceForSolution.java
+++ src/patterns/structural/wrappers/proxy/solution/PictureDownloadServiceForSolution.java
@@ -10,37 +10,19 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 public class PictureDownloadServiceForSolution implements Downloader {
-    private final String PREFIX = "https://w.forfun.com/fetch/";
+    private final PictureDownloaderServiceProxy downloader = new PictureDownloaderServiceProxy();
     private final PictureComparator comparator = new PictureComparator();
 
     @Override
     public void download(String path, String filename) {
-        if (invalid(path)) throw new IllegalArgumentException("Invalid url. Url must started at" + PREFIX);
-
-        System.err.println("Start download");
         long startTime = System.currentTimeMillis();
-        try {
-            Thread.sleep(5000);
-
-            URL url = new URL(path);
-
-            Path outputPath = Paths.get("src/patterns/wrappers/proxy/pictures/" + filename);
-
-            InputStream in = url.openStream();
-
-            Files.copy(in, outputPath, StandardCopyOption.REPLACE_EXISTING);
-
-        } catch (Exception e) {
-            System.err.println("In load time happened error");
-            e.printStackTrace();
+        if (comparator.arePictureEqual(path)) {
+            System.err.println("Start download");
+            long endTime = System.currentTimeMillis();
+            System.err.printf("Load finished with time %s %s%n", (endTime - startTime) / 1000, "seconds");
+        } else {
+            downloader.download(path, filename);
         }
-
-        long endTime = System.currentTimeMillis();
-        System.err.printf("Load finished with time %s %s%n", (endTime - startTime) / 1000, "seconds");
     }
 
-
-    private boolean invalid(String path) {
-        return !path.toLowerCase().startsWith(PREFIX);
-    }
 }
--- src/patterns/structural/wrappers/proxy/solution/PictureDownloaderServiceProxy.java
+++ src/patterns/structural/wrappers/proxy/solution/PictureDownloaderServiceProxy.java
@@ -2,20 +2,44 @@ package patterns.structural.wrappers.proxy.solution;
 
 import patterns.structural.wrappers.proxy.task.downloaders.validators.PictureComparator;
 
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
 public class PictureDownloaderServiceProxy implements Downloader {
-    private final PictureDownloadServiceForSolution downloader = new PictureDownloadServiceForSolution();
+
     private final PictureComparator comparator = new PictureComparator();
-
-
+    private final String PREFIX = "https://w.forfun.com/fetch/";
     @Override
     public void download(String path, String filename) {
+        if (invalid(path)) throw new IllegalArgumentException("Invalid url. Url must started at" + PREFIX);
+
+        System.err.println("Start download");
         long startTime = System.currentTimeMillis();
-        if (comparator.arePictureEqual(path)) {
-            System.err.println("Start download");
-            long endTime = System.currentTimeMillis();
-            System.err.printf("Load finished with time %s %s%n", (endTime - startTime) / 1000, "seconds");
-        } else {
-            downloader.download(path, filename);
+        try {
+            Thread.sleep(5000);
+
+            URL url = new URL(path);
+
+            Path outputPath = Paths.get("src/patterns/structural/wrappers/proxy/pictures/" + filename);
+
+            InputStream in = url.openStream();
+
+            Files.copy(in, outputPath, StandardCopyOption.REPLACE_EXISTING);
+
+        } catch (Exception e) {
+            System.err.println("In load time happened error");
+            e.printStackTrace();
         }
+
+        long endTime = System.currentTimeMillis();
+        System.err.printf("Load finished with time %s %s%n", (endTime - startTime) / 1000, "seconds");
+    }
+
+    private boolean invalid(String path) {
+        return !path.toLowerCase().startsWith(PREFIX);
     }
 }
